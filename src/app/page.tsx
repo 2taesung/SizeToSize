13631753535e8bf118a74f5353ebf3d163c9543b
root <root@DESKTOP-J22D89D>
2023-06-10 17:22:10 +0900

    chore: import/order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <main>sizetosize</main>;
+import Nav from '@src/components/Nav';
+import { useEffect } from 'react';
+
+export default function Page() {
+  useEffect(() => {
+    console.log('여기');
+  }, []);
+  return (
+    <main>
+      <Nav />
+      <h1>Hello, Home page!</h1>;
+    </main>
+  );
 }
